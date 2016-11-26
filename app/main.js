e86b412086796962758918300dc98b50131868a5
Qirong Ma <pmdiano@gmail.com>
2016-11-26 11:42:00 -0800

    Add TODO information

--- app/main.js
+++ app/main.js
@@ -3,5 +3,12 @@ import Router from 'react-router';
 import routes from './routes';
 
 Router.run(routes, Router.HistoryLocation, function(Handler) {
+  /**
+   * TODO: if use ReactDOM.render, it will throw following error:
+   * Uncaught Error: addComponentAsRefTo(...): Only a ReactOwner can have
+   * refs. You might be adding a ref to a component that was not created
+   * inside a component's `render` method, or you have multiple copies of
+   * React loaded (details: https://fb.me/react-refs-must-have-owner)
+   */
   React.render(<Handler />, document.getElementById('app'));
 });
